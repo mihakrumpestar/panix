7d01323b67871fe5f063980963a0b6576cdc4160
mihakrumpestar <70652456+mihakrumpestar@users.noreply.github.com>
2025-07-12 17:33:53 +0200

    added ping and some small fixes

--- internal/workflow/workflow.go
+++ internal/workflow/workflow.go
@@ -362,21 +362,6 @@ type machineInfo struct {
 	machine     config.Machine
 }
 
-func (w *WorkflowExecutor) rollbackMachines(machines []machineInfo) {
-	var wg sync.WaitGroup
-	for _, mi := range machines {
-		wg.Add(1)
-		go func(mi machineInfo) {
-			defer wg.Done()
-			err := w.executeMachineRollback(mi.flakeName, mi.configName, mi.machineName, mi.machine)
-			if err != nil {
-				fmt.Printf("Warning: rollback failed for machine %s: %v\n", mi.machineName, err)
-			}
-		}(mi)
-	}
-	wg.Wait()
-}
-
 func (w *WorkflowExecutor) executeMachinePreflight(flakeName, configName, machineName string, machine config.Machine) error {
 	return w.preflightMachine(flakeName, configName, machineName, machine)
 }
