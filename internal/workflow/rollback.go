0478851ba94a57c7bd5a3e023dee88492feac02f
mihakrumpestar <70652456+mihakrumpestar@users.noreply.github.com>
2025-07-14 21:56:04 +0200

    Embraced bubbletea and pond

--- internal/workflow/rollback.go
+++ internal/workflow/rollback.go
@@ -4,18 +4,19 @@ import (
 	"fmt"
 
 	"github.com/mihakrumpestar/panix/internal/config"
-	"github.com/mihakrumpestar/panix/internal/workflow/workflow_definition"
 )
 
-func (w *WorkflowExecutor) executeRollback(nextPhases []workflow_definition.WorkflowPhase) error {
-	return w.executeParallelMachines("rollback", w.executeMachineRollback, nextPhases)
-}
-
 func (w *WorkflowExecutor) executeMachineRollback(flakeName, configName, machineName string, machine *config.Machine) error {
-	// TODO: Implement secrets deployment
+	// TODO: Implement rollback functionality
 	if w.cfg.Global.Verbose {
-		fmt.Printf("Secrets deployment for machine %s/%s/%s: TODO - implement secrets deployment\n", flakeName, configName, machineName)
+		fmt.Printf("Rollback for machine %s/%s/%s: TODO - implement rollback\n", flakeName, configName, machineName)
 	}
+
+	// For now, just print what would be rolled back
+	// In a real implementation, this would:
+	// 1. Find the previous generation
+	// 2. Switch to the previous generation
+	// 3. Clean up the failed deployment
 
 	return nil
 }
--- internal/workflow/transfer.go
+++ internal/workflow/transfer.go
@@ -5,17 +5,11 @@ import (
 
 	"github.com/mihakrumpestar/panix/internal/config"
 	"github.com/mihakrumpestar/panix/internal/executioner"
-	"github.com/mihakrumpestar/panix/internal/workflow/workflow_definition"
 )
 
-func (w *WorkflowExecutor) executeTransfer(nextPhases []workflow_definition.WorkflowPhase) error {
-	return w.executeParallelMachines("transfer", w.transferToMachine, nextPhases)
-}
-
 // This function is called by executeMachineTransfer for individual machine transfers
-func (w *WorkflowExecutor) transferToMachine(flakeName, configName, machineName string, machine *config.Machine) error {
-	buildOutputPath := w.cfg.Flakes[flakeName].Configurations[configName].Metadata.BuildOutputPath
-	if buildOutputPath == "" {
+func (w *WorkflowExecutor) transferToMachine(flakeName, configName, machineName string, machine *config.Machine, cm *ConfigurationMetadata) error {
+	if cm.BuildOutputPath == "" {
 		return fmt.Errorf("machine %s/%s/%s has no build output path", flakeName, configName, machineName)
 	}
 
@@ -25,13 +19,13 @@ func (w *WorkflowExecutor) transferToMachine(flakeName, configName, machineName 
 	}
 
 	sshInfo := fmt.Sprintf("ssh://%s", machine.Ssh.Alias)
-	output, err := exc.Exec("nix", "copy", "--to", sshInfo, buildOutputPath)
+	output, err := exc.Exec("nix", "copy", "--to", sshInfo, cm.BuildOutputPath)
 	if err != nil {
 		return fmt.Errorf("nix copy failed: %w\n%s", err, output.Stderr.String())
 	}
 
 	if w.cfg.Global.Verbose {
-		fmt.Printf("Transferred %s to %s/%s/%s\n", buildOutputPath, flakeName, configName, machineName)
+		fmt.Printf("Transferred %s to %s/%s/%s\n", cm.BuildOutputPath, flakeName, configName, machineName)
 	}
 
 	return nil
