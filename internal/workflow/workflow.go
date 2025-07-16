a378a5f81abfaaa48106c4a1182e8f5c101864a3
mihakrumpestar <70652456+mihakrumpestar@users.noreply.github.com>
2025-07-16 23:26:42 +0200

    Implemented the new hook mechanism, but it is being blocked somewhere

--- internal/workflow/workflow.go
+++ internal/workflow/workflow.go
@@ -2,25 +2,36 @@ package workflow
 
 import (
 	"context"
-	"net/url"
+	"fmt"
+	"slices"
 
+	"github.com/alitto/pond/v2"
 	"github.com/mihakrumpestar/panix/internal/config"
+	"github.com/mihakrumpestar/panix/internal/executioner"
+	"github.com/mihakrumpestar/panix/internal/hook"
+	"github.com/pkg/errors"
 )
-
-type MetadataID struct {
-	FlakeName         string
-	ConfigurationName string
-	MachineName       *url.URL
-}
 
 type ActivationMetadata struct {
 	Error error
 }
 
 type WorkflowExecutor struct {
-	ctx    context.Context
-	cancel context.CancelFunc
-	cfg    *config.Config
+	ctx       context.Context
+	cancel    context.CancelFunc
+	cfg       *config.Config
+	metadatas *Metadatas
+	hook      *hook.Hook
+	pool      pond.Pool
+}
+
+type Metadatas struct {
+	*StatusMetadatas
+}
+
+type MetadatasBase struct {
+	groupPool pond.TaskGroup
+	Error     error
 }
 
 type WorkflowExecutorForConfigurationAndMachine struct {
@@ -28,30 +39,141 @@ type WorkflowExecutorForConfigurationAndMachine struct {
 	cfg *config.Global
 }
 
-func NewWorkflowExecutor(ctx context.Context, cfg *config.Config) (*WorkflowExecutor, error) {
+func NewWorkflowExecutor(ctx context.Context, cfg *config.Config) *WorkflowExecutor {
 	ctx, cancel := context.WithTimeout(ctx, cfg.Global.Timeout)
 
+	hookI := hook.NewHook()
+
 	return &WorkflowExecutor{
-		ctx:    ctx,
-		cancel: cancel,
-		cfg:    cfg,
-	}, nil
+		ctx:       ctx,
+		cancel:    cancel,
+		cfg:       cfg,
+		metadatas: &Metadatas{},
+		hook:      hookI,
+		pool:      pond.NewPool(cfg.Global.Concurrency),
+	}
+}
+
+func (w *WorkflowExecutor) Metadatas() *Metadatas {
+	return w.metadatas
+}
+
+func (w *WorkflowExecutor) Done() <-chan uint64 {
+	return w.hook.Done()
 }
 
 // Helpers
 
-func forAllMachines(conf map[string]*config.Flake, function func(i int, flakeName, configurationName string, machineName *url.URL, machine *config.Machine)) {
-	i := 0
-	for flakeName, flake := range conf {
+func (w *WorkflowExecutor) forEachFlakeConfiguration(msBase *MetadatasBase, function func(wp *WorkflowExecutorForConfigurationAndMachine, bm *executioner.BaseMetadata, configuration *config.Configuration) error) error {
+	if msBase == nil {
+		msBase = &MetadatasBase{}
+	}
+
+	groupPool := msBase.groupPool
+	if groupPool == nil {
+		groupPool = w.pool.NewGroupContext(w.ctx)
+	}
+
+	errCacher := make([]error, 0)
+
+	for flakeName, flake := range w.cfg.Flakes {
 		for configurationName, configuration := range flake.Configurations {
-			for machineName, machine := range configuration.Machines {
-				function(i, flakeName, configurationName, &machineName, machine)
-				i++
+			wp := &WorkflowExecutorForConfigurationAndMachine{w.ctx, &w.cfg.Global}
+
+			bm := &executioner.BaseMetadata{
+				MetadataID: executioner.MetadataID{
+					FlakeName:         flakeName,
+					ConfigurationName: configurationName,
+				},
+			}
+
+			if w.cfg.Global.RequireAllSuccess {
+				groupPool.SubmitErr(func() error {
+					err := function(wp, bm, configuration)
+					errCacher = append(errCacher, err)
+					return err
+				})
+			} else {
+				groupPool.Submit(func() {
+					err := function(wp, bm, configuration)
+					errCacher = append(errCacher, err)
+				})
 			}
 		}
 	}
+
+	err := groupPool.Wait()
+
+	if err != nil {
+		msBase.Error = errors.Wrapf(err, "phase failed because of 'RequireAllSuccess'")
+		w.hook.OnUpdateHook()
+		return msBase.Error
+	}
+
+	if !slices.Contains(errCacher, nil) {
+		msBase.Error = fmt.Errorf("phase failed because of all machines failed this phase")
+		w.hook.OnUpdateHook()
+		return msBase.Error
+	}
+
+	return nil
 }
 
+func (w *WorkflowExecutor) forEachConfigurationMachine(configuration *config.Configuration, msBase *MetadatasBase, bm *executioner.BaseMetadata, function func(wp *WorkflowExecutorForConfigurationAndMachine, bm *executioner.BaseMetadata, machine *config.Machine) error) error {
+	groupPool := msBase.groupPool
+	if groupPool == nil {
+		groupPool = w.pool.NewGroupContext(w.ctx)
+	}
+
+	errCacher := make([]error, 0)
+
+	for machineName, machine := range configuration.Machines {
+		wp := &WorkflowExecutorForConfigurationAndMachine{w.ctx, &w.cfg.Global}
+
+		bm.MetadataID.MachineName = &machineName
+
+		if w.cfg.Global.RequireAllSuccess {
+			groupPool.SubmitErr(func() error {
+				err := function(wp, bm, machine)
+				errCacher = append(errCacher, err)
+				return err
+			})
+		} else {
+			groupPool.Submit(func() {
+				err := function(wp, bm, machine)
+				errCacher = append(errCacher, err)
+			})
+		}
+	}
+
+	err := groupPool.Wait()
+	if err != nil {
+		msBase.Error = errors.Wrapf(err, "phase failed because of 'RequireAllSuccess'")
+		w.hook.OnUpdateHook()
+		return msBase.Error
+	}
+
+	// Only wait for Done() if no error occurred
+	select {
+	case <-w.metadatas.StatusMetadatas.groupPool.Done():
+		// Continue with normal flow
+	case <-w.ctx.Done():
+		// Context cancelled, exit early
+		msBase.Error = w.ctx.Err()
+		w.hook.OnUpdateHook()
+		return msBase.Error
+	}
+
+	if !slices.Contains(errCacher, nil) {
+		msBase.Error = fmt.Errorf("phase failed because of all machines failed this phase")
+		w.hook.OnUpdateHook()
+		return msBase.Error
+	}
+
+	return nil
+}
+
+/*
 func forAllConfigurations(conf map[string]*config.Flake, sms []StatusMetadata, function func(flakeName, configurationName string, flake *config.Flake, configuration *config.Configuration)) {
 	for flakeName, flake := range conf {
 		for configurationName, configuration := range flake.Configurations {
@@ -73,3 +195,4 @@ func forAllConfigurations(conf map[string]*config.Flake, sms []StatusMetadata, f
 		}
 	}
 }
+*/
