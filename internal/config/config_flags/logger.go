2774777d52f6fa94d16f63d49477038d1fe46b05
mihakrumpestar <70652456+mihakrumpestar@users.noreply.github.com>
2026-02-24 10:39:04 +0100

    fix: Prevent logging because of default log file

--- internal/config/config_flags/logger.go
+++ internal/config/config_flags/logger.go
@@ -11,7 +11,7 @@ import (
 
 func InitLogging(flags Logging) error {
 	// Determine if logging should be enabled
-	enabled := flags.Log || flags.Debug || flags.LogFile != ""
+	enabled := flags.Log || flags.Debug
 
 	if !enabled {
 		// Logging is disabled - set global level to disable all logging
