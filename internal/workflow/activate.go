8ebe1d45ceecc7d248e001bc8c87dda0bcb10a09
mihakrumpestar <70652456+mihakrumpestar@users.noreply.github.com>
2026-03-06 19:33:37 +0100

    refactor: golangci nestif

--- internal/workflow/activate.go
+++ internal/workflow/activate.go
@@ -14,80 +14,102 @@ func (w *Workflow) executeActivatePhaseMachine(machine *config.Machine) error {
 			systemClosure := machine.ParentConfiguration.MetaBuild.SystemClosure
 
 			if !machine.MetaInspect.Bootstrapped.Load() && !w.conf.Flags.Bootstrap.DisableAuto {
-				err := exc.Exec(
-					"nixos-install",
-					"installing NixOS",
-					"nixos-install failed",
-					[]string{"nixos-install", "--no-root-passwd", "--no-channel-copy", "--system", systemClosure, "--root", "/mnt"},
-				)
-				if err != nil {
-					return errors.Wrap(err, "nixos-install failed")
-				}
-
-				if len(machine.Bootstrap.PostBootstrapInstallHooks) > 0 {
-					err = exc.ExecuteHooks(machine.Bootstrap.PostBootstrapInstallHooks, "post bootstrap install hook")
-					if err != nil {
-						return errors.Wrap(err, "post bootstrap install hooks failed")
-					}
-				}
-
-				if !machine.Bootstrap.DisableAutomaticReboot {
-					err = exc.Exec(
-						"reboot",
-						"rebooting",
-						"reboot failed",
-						[]string{"reboot"},
-					)
-					if err != nil {
-						return errors.Wrap(err, "reboot failed")
-					}
-				}
-
-				if len(machine.Bootstrap.PostBootstrapProvisionedHooks) > 0 {
-					if !machine.Bootstrap.DisableAutomaticReboot {
-						activeSSH := machine.MetaInspect.GetActiveSSH()
-						err = executioner.WaitForDisconnect(exc, activeSSH, "waiting for machine to reboot")
-
-						if err != nil {
-							return errors.Wrap(err, "wait for disconnect failed")
-						}
-
-						machine.SwitchToRegularSSH()
-						activeSSH = machine.MetaInspect.GetActiveSSH()
-
-						err = executioner.WaitForReconnect(exc, activeSSH, "waiting for machine to come back online", "machine did not reconnect after reboot")
-						if err != nil {
-							return errors.Wrap(err, "wait for reconnect failed")
-						}
-					}
-
-					return exc.ExecuteHooks(machine.Bootstrap.PostBootstrapProvisionedHooks, "post bootstrap provisioned hook")
-				}
-			} else {
-				err := exc.Exec(
-					"add system closure to profiles",
-					"updating system profile",
-					"setting system closure to profiles failed",
-					append(machine.MaybeSudo(), "nix-env", "--profile", "/nix/var/nix/profiles/system", "--set", systemClosure),
-				)
-				if err != nil {
-					return errors.Wrap(err, "nix-env set system profile failed")
-				}
-
-				binPath := systemClosure + "/bin/switch-to-configuration"
-
-				err = exc.Exec(
-					"activate",
-					"activating configuration",
-					"activation failed",
-					append(machine.MaybeSudo(), binPath, "switch"),
-				)
-				if err != nil {
-					return errors.Wrap(err, "activation failed")
-				}
+				return executeBootstrap(exc, machine, systemClosure)
 			}
 
-			return nil
+			return executeActivation(exc, machine, systemClosure)
 		},
 	)
+}
+
+func executeBootstrap(exc *executioner.Executioner, machine *config.Machine, systemClosure string) error {
+	err := exc.Exec(
+		"nixos-install",
+		"installing NixOS",
+		"nixos-install failed",
+		[]string{"nixos-install", "--no-root-passwd", "--no-channel-copy", "--system", systemClosure, "--root", "/mnt"},
+	)
+	if err != nil {
+		return errors.Wrap(err, "nixos-install failed")
+	}
+
+	if len(machine.Bootstrap.PostBootstrapInstallHooks) > 0 {
+		err = exc.ExecuteHooks(machine.Bootstrap.PostBootstrapInstallHooks, "post bootstrap install hook")
+		if err != nil {
+			return errors.Wrap(err, "post bootstrap install hooks failed")
+		}
+	}
+
+	if !machine.Bootstrap.DisableAutomaticReboot {
+		if err := performReboot(exc, machine); err != nil {
+			return err
+		}
+	}
+
+	if len(machine.Bootstrap.PostBootstrapProvisionedHooks) > 0 {
+		if err := exc.ExecuteHooks(machine.Bootstrap.PostBootstrapProvisionedHooks, "post bootstrap provisioned hook"); err != nil {
+			return errors.Wrap(err, "post bootstrap provisioned hooks failed")
+		}
+	}
+
+	return nil
+}
+
+func performReboot(exc *executioner.Executioner, machine *config.Machine) error {
+	err := exc.Exec(
+		"reboot",
+		"rebooting",
+		"reboot failed",
+		[]string{"reboot"},
+	)
+	if err != nil {
+		return errors.Wrap(err, "reboot failed")
+	}
+
+	if len(machine.Bootstrap.PostBootstrapProvisionedHooks) == 0 {
+		return nil
+	}
+
+	activeSSH := machine.MetaInspect.GetActiveSSH()
+
+	err = executioner.WaitForDisconnect(exc, activeSSH, "waiting for machine to reboot")
+	if err != nil {
+		return errors.Wrap(err, "wait for disconnect failed")
+	}
+
+	machine.SwitchToRegularSSH()
+	activeSSH = machine.MetaInspect.GetActiveSSH()
+
+	err = executioner.WaitForReconnect(exc, activeSSH, "waiting for machine to come back online", "machine did not reconnect after reboot")
+	if err != nil {
+		return errors.Wrap(err, "wait for reconnect failed")
+	}
+
+	return nil
+}
+
+func executeActivation(exc *executioner.Executioner, machine *config.Machine, systemClosure string) error {
+	err := exc.Exec(
+		"add system closure to profiles",
+		"updating system profile",
+		"setting system closure to profiles failed",
+		append(machine.MaybeSudo(), "nix-env", "--profile", "/nix/var/nix/profiles/system", "--set", systemClosure),
+	)
+	if err != nil {
+		return errors.Wrap(err, "nix-env set system profile failed")
+	}
+
+	binPath := systemClosure + "/bin/switch-to-configuration"
+
+	err = exc.Exec(
+		"activate",
+		"activating configuration",
+		"activation failed",
+		append(machine.MaybeSudo(), binPath, "switch"),
+	)
+	if err != nil {
+		return errors.Wrap(err, "activation failed")
+	}
+
+	return nil
 }
